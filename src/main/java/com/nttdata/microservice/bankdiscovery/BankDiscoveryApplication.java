6bbbfd33d71462d5ed009c794d0fa019afd9ee51
csanmont <csanmont@emeal.nttdata.com>
2022-11-30 22:01:43 -0500

    configuracion inicial

---
+++ src/main/java/com/nttdata/microservice/bankdiscovery/BankDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.microservice.bankdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankDiscoveryApplication.class, args);
+	}
+
+}
